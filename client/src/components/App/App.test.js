348f14ec71e0b09833a1ea616b18f91753bea103
Shannon Largman <shannon.largman@gmail.com>
2022-05-19 20:44:30 -0700

    Add placeholder tests

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,15 +1,5 @@
-import { unmountComponentAtNode } from "react-dom";
-
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
+describe('App', () => {
+  it('should be happy', () => {
+    expect(true).toBe(true);
+  });
 });
