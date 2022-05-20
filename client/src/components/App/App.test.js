607cfcaa55b0130918bc922d8f37398c06ccbea1
Shannon Largman <shannon.largman@gmail.com>
2022-05-19 17:04:32 -0700

    Add test setup/teardown

---
+++ client/src/components/App/App.test.js
@@ -0,0 +1,15 @@
+import { unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
