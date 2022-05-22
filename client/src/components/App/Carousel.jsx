9bb1ef89196e3f59f57630ea26bd5370e14af6b0
Shannon Largman <shannon.largman@gmail.com>
2022-05-22 16:30:38 -0700

    Create basic carousel component

---
+++ client/src/components/App/Carousel.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+function Carousel({ items, size }) {
+  const range = (start, end) =>
+    Array.from(Array(Math.max(0, start - end)), (_, i) => start + i);
+  const [start, setStart] = useState(0);
+
+  const decrement = (event) => {
+    event.preventDefault();
+    setStart((oldStart) => Math.max(0, oldStart - size));
+  };
+
+  const increment = (event) => {
+    event.preventDefault();
+    setStart((oldStart) => Math.min(items.length - size + 1, oldStart + size));
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={decrement}>
+        &lt;
+      </button>
+      {items.slice(start, start + size)}
+      <button type="button" onClick={increment}>
+        &gt;
+      </button>
+    </div>
+  );
+}
+
+Carousel.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.element).isRequired,
+  size: PropTypes.number.isRequired,
+};
+
+export default Carousel;
