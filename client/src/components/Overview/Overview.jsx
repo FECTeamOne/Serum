a731ded64b66d09b3badf77314ccea0a8ec73656
Shannon Largman <shannon.largman@gmail.com>
2022-05-22 18:15:16 -0700

    Set up data flow in Overview

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import ImageGallery from 'Overview/ImageGallery.jsx';
 import ProductInformation from 'Overview/ProductInformation.jsx';
+import testData from '../../testData.js';
 
-function Overview() {
+function Overview({ productId }) {
+  const [product, setProduct] = useState({});
+  const [styles, setStyles] = useState({});
+  const [selectedStyle, setSelectedStyle] = useState();
+
+  useEffect(async () => {
+    // TODO: account for unmounting
+    try {
+      const [productResponse, stylesResponse] = await Promise.all([
+        axios.get(`/products/${productId}`),
+        axios.get(`/products/${productId}/styles`),
+      ]);
+
+      setProduct(productResponse.data);
+      setStyles(stylesResponse.data);
+    } catch (error) {
+      // TODO: handle error
+    }
+  }, []);
+
+  const handleStyleSelect = (styleId) => {
+    setSelectedStyle(styleId);
+  };
+
   return (
     <div>
-      <ImageGallery />
-      <ProductInformation />
+      <ImageGallery
+        // selectedStyle={selectedStyle}
+        selectedStyle={1}
+      />
+      <ProductInformation
+        // product={product}
+        // styles={styles}
+        product={testData.product}
+        styles={testData.styles}
+        handleStyleSelect={handleStyleSelect}
+      />
     </div>
   );
 }
+
+Overview.propTypes = {
+  productId: PropTypes.number.isRequired,
+};
 
 export default Overview;
