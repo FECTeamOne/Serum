b66211db39a402fad878e2cb3736aaa9e05d457f
zman811 <sunburnzman@gmail.com>
2022-05-23 13:31:00 -0600

    Added in basic buttons for adding a review as well as seeing more

--- client/src/components/Reviews/components/ReviewList.jsx
+++ client/src/components/Reviews/components/ReviewList.jsx
@@ -12,19 +12,39 @@ const List = styled.div`
   margin-top: 1em;
 `;
 
-const Buttons = styled.button`
+const AddReview = styled.button`
+  background: none!important;
+  border: none;
+  padding: 0!important;
   position: absolute;
-  right:  20%;
+  right:  40%;
+  bottom:  2%;
+`;
+const MoreReviews = styled.button`
+  background: none!important;
+  border: none;
+  padding: 0!important;
+  position: absolute;
+  right:  60%;
   bottom:  2%;
 `;
 
 function ReviewList() {
+  function AddReview(e) {
+    e.preventDefault();
+    // TODO this will pull up the review modal window to allow for the user to add a review
+  }
+  function MoreReviews(e) {
+    e.preventDefault();
+    // TODO make a request to the api to get more/will change which reviews are being displayed
+  }
   return (
     <List>
       <div>there are some reviews that are not sorted</div>
       <Review />
       <Review />
-      <Buttons>Test</Buttons>
+      <AddReview onClick={AddReview}> Add review</AddReview>
+      <MoreReviews onClick={MoreReviews}>More reviews</MoreReviews>
     </List>
   );
 }
