bb4041459da4989aa90d85d680bda02c98e1a475
Shannon Largman <shannon.largman@gmail.com>
2022-05-22 17:05:27 -0700

    Clean up App

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -3,24 +3,6 @@ import styled, { css } from 'styled-components';
 import Reviews from 'Reviews/Reviews.jsx';
 import Overview from 'Overview/Overview.jsx';
 
-// a very basic example of how to use the styles
-// It uses `` as a function call then you put in any css
-const Button = styled.button`
-background: transparent;
-border-radius: 3px;
-border: 2px solid palevioletred;
-color: palevioletred;
-margin: 0.5em 1em;
-padding: 0.25em 1em;
-
-/* this is to change style based on a prop */
-
-${(props) => props.test && css`
-  background: palevioletred;
-  color: white;
-`};
-`;
-
 const Container = styled.div`
   text-align: center;
 `;
@@ -28,9 +10,6 @@ const Container = styled.div`
 function App() {
   return (
     <Container>
-      Hello
-      <Button as="a" href="#">test</Button>
-      <Button test>test</Button>
       <Overview />
       <Reviews />
     </Container>
