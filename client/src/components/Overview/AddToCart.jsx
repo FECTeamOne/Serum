b1851f5e588269721a1d02544b282d33dfcfe740
Shannon Largman <shannon.largman@gmail.com>
2022-05-23 08:40:01 -0700

    Add quantity selector
    Also fixed some bugs with the size selector. In order for the size
    selector to be updated when the styles changed we need to useEffect
    suscribed to the skus prop. However, in the future we will want to
    change the initial selectedSize to "Select Size" which will remove the
    need for that subscription.
    
    In principle quantities should not be state since it can be computed
    from the selectedSize. However, in order to get the <Select /> for
    quantity to show the correct options I added it to state to trigger a
    rerender that would include the updated value for quantities. Otherwise,
    the <Select /> is empty, maybe due to the async nature of useEffect.

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -1,32 +1,59 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+
+function Select({ value, options = [], onChange }) {
+  return (
+    <select
+      value={value}
+      onChange={onChange}
+    >
+      {options.map(option => (
+        <option key={option} value={option}>{option}</option>
+      ))}
+    </select>
+  );
+}
 
 function AddToCart({ skus }) {
   const [selectedSize, setSelectedSize] = useState();
+  // should quantities be state? it's computed from selectedSize and i only added it to state so updates to it would trigger a rerender and update the <Select /> for quantity
+  const [quantities, setQuantities] = useState([1]);
   const [selectedQuantity, setSelectedQuantity] = useState();
 
-  const sizes = Object.values(skus).map(sku => (
-    sku.size
-  ));
+  const sizes = Object.values(skus).map(sku => sku.size);
 
-  const handleSizeChange = (size) => {
-    setSelectedSize(size);
+  useEffect(() => {
+    setSelectedSize(Object.values(skus)[0].size);
+  }, [skus]);
+
+  useEffect(() => {
+    if (selectedSize) {
+      const maxQuantity = Object.values(skus).find(sku => sku.size === selectedSize).quantity;
+      setQuantities(Array(Math.min(maxQuantity, 15)).fill().map((_, i) => i + 1));
+      setSelectedQuantity(1);
+    }
+  }, [selectedSize]);
+
+  const handleSizeChange = (event) => {
+    setSelectedSize(event.target.value);
   };
 
-  const handleQuantityChange = (quantity) => {
-    setSelectedQuantity(quantity);
+  const handleQuantityChange = (event) => {
+    setSelectedQuantity(event.target.value);
   };
 
   return (
     <form>
-      <select
+      <Select
         value={selectedSize}
+        options={sizes}
         onChange={handleSizeChange}
-      >
-        {sizes.map(size => (
-          <option value={size}>{size}</option>
-        ))}
-      </select>
+      />
+      <Select
+        value={selectedQuantity}
+        options={quantities}
+        onChange={handleQuantityChange}
+      />
     </form>
   );
 }
