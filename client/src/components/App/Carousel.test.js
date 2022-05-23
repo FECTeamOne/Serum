94504c457caa558de4a27c0ea7f1e632019dca05
Shannon Largman <shannon.largman@gmail.com>
2022-05-23 16:34:44 -0700

    Rearrange test setup

--- client/src/components/App/Carousel.test.js
+++ client/src/components/App/Carousel.test.js
@@ -5,14 +5,7 @@ import Carousel from 'App/Carousel.jsx';
 
 describe('Carousel', () => {
 
-  it('should show the correct number of items', () => {
-    const numbers = Array(10).fill().map((_, i) => i);
-    const items = numbers.map(i => (
-      <div key={i}>{i}</div>
-    ));
-    const size = 4;
-
-    const getScreenItems = () => {
+  const getScreenItems = () => {
       const screenItems = [];
       numbers.forEach(i => {
         const screenItem = screen.queryByText(new RegExp(`^${i}$`));
@@ -21,12 +14,23 @@ describe('Carousel', () => {
         }
       });
       return screenItems;
-    }
+    };
+
+  beforeEach(() => {
+    const numbers = Array(10).fill().map((_, i) => i);
+    const items = numbers.map(i => (
+      <div key={i}>{i}</div>
+    ));
+    const size = 4;
 
     render(
       <Carousel items={items} size={size} />
     );
 
+  });
+
+  it('should show the correct number of items', () => {
+    
     let screenItems = getScreenItems();
     expect(screenItems.length).toEqual(size);
   });
