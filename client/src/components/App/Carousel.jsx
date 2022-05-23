5872b52658a22848ac24cd0ecbf47a0bcc8b2976
Shannon Largman <shannon.largman@gmail.com>
2022-05-22 18:45:48 -0700

    Add product details to ProductInformation

--- client/src/components/App/Carousel.jsx
+++ client/src/components/App/Carousel.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Display a set of item with buttons to scroll through them
