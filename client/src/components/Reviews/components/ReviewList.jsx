c57a996647ca1d9d8d409a5b96a2cf23abbedda7
zman811 <sunburnzman@gmail.com>
2022-05-23 15:34:45 -0600

    Use state for sort dropdown

--- client/src/components/Reviews/components/ReviewList.jsx
+++ client/src/components/Reviews/components/ReviewList.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Review from 'Reviews/components/Review.jsx';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const List = styled.div`
   position: relative;
@@ -30,8 +31,19 @@ const MoreReviewsButton = styled.button`
   bottom:  2%;
   cursor: pointer;
 `;
+const StyledSelect = styled.select`
+  border: none;
+`;
 
 function ReviewList() {
+  const [currentSort, setCurrentSort] = useState('relevant');
+  useEffect(() => {
+    // TODO axios.get() with currentSort param
+  }, [currentSort]);
+  function HandleSort(e) {
+    e.preventDefault();
+    setCurrentSort(e.target.value);
+  }
   function HandleAddReview(e) {
     e.preventDefault();
     // TODO this will pull up the review modal window to allow for the user to add a review
@@ -42,7 +54,14 @@ function ReviewList() {
   }
   return (
     <List>
-      <div>there are some reviews that are not sorted</div>
+      <form>
+        Sort by
+        <StyledSelect value={currentSort} onChange={HandleSort}>
+          <option value="relevant">Relevant</option>
+          <option value="newest">Newest</option>
+          <option value="helpful">Helpful</option>
+        </StyledSelect>
+      </form>
       <Review />
       <Review />
       <AddReviewButton onClick={HandleAddReview}> Add review</AddReviewButton>
