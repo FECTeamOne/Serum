3cfc444b8cd95b7d4a867203a1b23178eb4a627d
zman811 <sunburnzman@gmail.com>
2022-05-23 16:22:13 -0600

    Fixed some var names

--- client/src/components/Reviews/components/ReviewList.jsx
+++ client/src/components/Reviews/components/ReviewList.jsx
@@ -40,15 +40,14 @@ function ReviewList() {
   useEffect(() => {
     // TODO axios.get() with currentSort param
   }, [currentSort]);
-  function HandleSort(e) {
-    e.preventDefault();
+  function handleSort(e) {
     setCurrentSort(e.target.value);
   }
-  function HandleAddReview(e) {
+  function handleAddReview(e) {
     e.preventDefault();
     // TODO this will pull up the review modal window to allow for the user to add a review
   }
-  function HandleMoreReviews(e) {
+  function handleMoreReviews(e) {
     e.preventDefault();
     // TODO make a request to the api to get more/will change which reviews are being displayed
   }
@@ -56,7 +55,7 @@ function ReviewList() {
     <List>
       <form>
         Sort by
-        <StyledSelect value={currentSort} onChange={HandleSort}>
+        <StyledSelect value={currentSort} onChange={handleSort}>
           <option value="relevant">Relevant</option>
           <option value="newest">Newest</option>
           <option value="helpful">Helpful</option>
@@ -64,8 +63,8 @@ function ReviewList() {
       </form>
       <Review />
       <Review />
-      <AddReviewButton onClick={HandleAddReview}> Add review</AddReviewButton>
-      <MoreReviewsButton onClick={HandleMoreReviews}>More reviews</MoreReviewsButton>
+      <AddReviewButton onClick={handleAddReview}> Add review</AddReviewButton>
+      <MoreReviewsButton onClick={handleMoreReviews}>More reviews</MoreReviewsButton>
     </List>
   );
 }
