be5965dc838345a9df3dba5c0c517dbcb25a3547
zman811 <sunburnzman@gmail.com>
2022-05-23 14:09:07 -0600

    Added in a library to parse the date as well as reformated some things

--- client/src/components/Reviews/components/Review.jsx
+++ client/src/components/Reviews/components/Review.jsx
@@ -1,6 +1,7 @@
 // may add a diffrent file for helfult/report buttons
 import React from 'react';
 import styled from 'styled-components';
+import { formatDistanceToNow, parseISO } from 'date-fns';
 
 const StyledReview = styled.div`
   border: 2px solid black;
@@ -20,28 +21,44 @@ const Title = styled.h2`
   padding-top: 10px;
   font-size: 150%;
 `;
+const Buttons = styled.button`
+  background: none!important;
+  border: none;
+  cursor: pointer;
+`;
 
 function Review({ review }) {
+  let date;
+  let rec;
   function ReviewClick(e, type) {
+    console.log('clicked')
     e.preventDefault();
     // send request to review.review_id, type (helpful or report)
   }
+  function PareseData() {
+    date = formatDistanceToNow(parseISO(review.date));
+    if (review.recommend) {
+      rec = '✅';
+    }
+  }
+  PareseData();
   return (
     <StyledReview>
       <Stars>{review.rating}</Stars>
       {/* need to add fn to display stars */}
       <User>
+        {rec}
         {review.reviewer_name}
-        ,
-        {review.date}
-        {/* need to parse data */}
+        {`, ${date} ago`}
       </User>
       <br />
       <Title>{review.summary}</Title>
       <br />
       <p>{review.body}</p>
-      <button type="button" onClick={(e) => ReviewClick(e, 'helpful')}> Helpful? # |</button>
-      <button type="button" onClick={(e) => ReviewClick(e, 'report')}>| Report</button>
+      <Buttons onClick={(e) => ReviewClick(e, 'helpful')}>
+        {`Helpful? ${review.helpfulness}`}
+      </Buttons>
+      <Buttons type="button" onClick={(e) => ReviewClick(e, 'report')}>| Report</Buttons>
     </StyledReview>
   );
 }
