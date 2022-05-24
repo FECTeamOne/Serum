b59d28939acdad5d0f0903a7e5660ba8a81929b9
zman811 <sunburnzman@gmail.com>
2022-05-24 10:01:45 -0600

    Added some basic tests

--- client/src/components/Reviews/components/ReviewList.test.js
+++ client/src/components/Reviews/components/ReviewList.test.js
@@ -1,10 +1,23 @@
-import {render, fireEvent, waitFor, screen} from '@testing-library/react';
-import App from '../../App/App.jsx';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import ReviewList from 'Reviews/components/ReviewList.jsx';
 
-descrbe('test', () => {
+describe('test', () => {
   test('Ratings & Reviews', async () => {
-    render(<App />);
+    const user = userEvent.setup();
+    render(<ReviewList />);
 
-    expect(screen.getByRole('button')).not.toBeDisabled();
+    await user.click(screen.getByText('More reviews'));
+
+    expect(screen.getByText('More reviews')).not.toBeDisabled();
+  });
+  test('Ratings & Reviews helpful', async () => {
+    const user = userEvent.setup();
+    render(<ReviewList />);
+
+    await user.click(screen.getAllByText('| Report')[0]);
+    await user.click(screen.getAllByText('| Report')[1]);
+
+    expect(screen.getAllByText('| Report')[0]).not.toBeDisabled();
+    expect(screen.getAllByText('| Report')[1]).not.toBeDisabled();
   });
 })
