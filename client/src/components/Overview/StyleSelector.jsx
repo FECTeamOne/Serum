0b0b80bc194d8ba6be5c2d5688c8d90ca1c84f8d
Shannon Largman <Shannon.largman@gmail.com>
2022-05-25 10:51:42 -0700

    Style selector basic functionality (#76)
    * Add prop-types package
    
    * Create basic carousel component
    
    * Remove unnecessary range function
    
    * Clean up App
    
    * Remove empty Carousel test file
    
    * Set up data flow in Overview
    
    * Add product details to ProductInformation
    
    * Make ProductInformation props required
    
    * Fix props for StyleSelector
    
    * Use proper async useEffect pattern
    
    * Select style by ID
    
    Optional chaining is used to avoid errors resulting from lookups on
    undefined.
    
    * Add basic style selector functionality
    
    * Add product details to ProductInformation
    
    * Select style by ID
    
    Optional chaining is used to avoid errors resulting from lookups on
    undefined.
    
    * Add basic style selector functionality
    
    * Remove merge artifacts
    
    * Test for style change on thumbnail click
    
    * Move StyleSelector and AddToCart to Overview
    
    * Simplify StyleSelector
    
    Also makes changes for accessibility.

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function StyleSelector() {
-  return null;
+/**
+ * Displays thumbnails of the product styles and
+ * allows style selection by clicking on a thumbnail
+ */
+function Style({ style, handleStyleSelect, selected }) {
+  // TODO: handle selected overlay
+  // TODO:
+  return (
+    <button
+      type="button"
+      onClick={() => handleStyleSelect(style.style_id)}
+    >
+      <img
+        src={style.photos[0].thumbnail_url}
+        alt={`${style.name} style thumbnail`}
+        width="50"
+      />
+    </button>
+  );
 }
+
+Style.propTypes = {
+  style: PropTypes.object.isRequired,
+  handleStyleSelect: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
+};
+
+function StyleSelector({ styles, selectedStyleId, handleStyleSelect }) {
+  // TODO: use color-thief to extract color of thumbnails server-side
+  // or parse style name
+  return (
+    <div>
+      {styles.map((style) => (
+        <Style
+          key={style.style_id}
+          style={style}
+          handleStyleSelect={handleStyleSelect}
+          selected={style.style_id === selectedStyleId}
+        />
+      ))}
+    </div>
+  );
+}
+
+StyleSelector.propTypes = {
+  styles: PropTypes.array.isRequired,
+  selectedStyleId: PropTypes.number.isRequired,
+  handleStyleSelect: PropTypes.func.isRequired,
+};
 
 export default StyleSelector;
