4b38fef30be14d01119b6365a4d72357bc810ed0
Shannon Largman <shannon.largman@gmail.com>
2022-05-24 21:06:09 -0700

    Test for correct sizes in selector

--- client/src/tests/testSetup.js
+++ client/src/tests/testSetup.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, getAllByRole } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import testData from 'tests/testData.js';
 
 global.React = React;
 global.render = render;
+global.getAllByRole = getAllByRole;
 global.userEvent = userEvent;
 global.testData = testData;
