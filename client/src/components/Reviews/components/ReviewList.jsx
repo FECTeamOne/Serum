54b30a2b611c883a0c618b721bd1cb63fbc5f48c
Zachary Kessler <sunburnzman@gmail.com>
2022-05-25 13:10:57 -0600

    Review modal (#92)
    * Start on add review modal added recommend button and started on next
    
    * Trying to get size to work
    
    * Got the next button working with test
    
    * Working but needs to be changed
    
    * Reformated state
    
    * Unstyled review add modal
    
    * Using same data structure as with real data
    
    * Added interactive stars
    
    * Made some changes in review that I just found out about
    
    * Trying to get img to upload
    
    * Started work on submission
    
    * Changes some var names
    
    Co-authored-by: Shannon Largman <Shannon.largman@gmail.com>

--- client/src/components/Reviews/components/ReviewList.jsx
+++ client/src/components/Reviews/components/ReviewList.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Review from 'Reviews/components/Review.jsx';
+import AddReview from 'Reviews/components/AddReview.jsx';
 import styled from 'styled-components';
-import { reviews } from 'tests/testData.js'
+import { reviews, reviewsMetadata } from 'tests/testData.js'
 import axios from 'axios';
 
 const List = styled.div`
@@ -38,19 +39,28 @@ const StyledSelect = styled.select`
 
 function ReviewList() {
   const [currentSort, setCurrentSort] = useState('relevant');
+  const [toggleModal, setToggleModal] = useState(false);
   useEffect(() => {
     // TODO axios.get() with currentSort param
   }, [currentSort]);
-  function handleSort(e) {
+  const handleSort = (e) => {
     setCurrentSort(e.target.value);
-  }
-  function handleAddReview(e) {
-    e.preventDefault();
+  };
+  const handleModalToggle = () => {
+    setToggleModal(!toggleModal);
     // TODO this will pull up the review modal window to allow for the user to add a review
-  }
-  function handleMoreReviews(e) {
-    e.preventDefault();
+  };
+  const handleMoreReviews = () => {
     // TODO make a request to the api to get more/will change which reviews are being displayed
+  };
+  if (toggleModal) {
+    const productCharacteristics = Object.keys(reviewsMetadata.characteristics);
+    return (
+      <AddReview
+        handleModalToggle={handleModalToggle}
+        productCharacteristics={productCharacteristics}
+      />
+    );
   }
   return (
     <List>
@@ -63,7 +73,7 @@ function ReviewList() {
         </StyledSelect>
       </form>
       {reviews.results.map((review) => <Review review={review} key={review.review_id} />)}
-      <AddReviewButton onClick={handleAddReview}> Add review</AddReviewButton>
+      <AddReviewButton onClick={handleModalToggle}> Add review</AddReviewButton>
       <MoreReviewsButton onClick={handleMoreReviews}>More reviews</MoreReviewsButton>
     </List>
   );
