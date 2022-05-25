9c62ee0e5209fc5ed995f2ec5fdbb83c59a27c8f
Zachary Kessler <sunburnzman@gmail.com>
2022-05-25 17:35:42 -0600

    Added in stars component in app (#96)
    Added in stars component in app

--- client/src/components/Reviews/components/Review.jsx
+++ client/src/components/Reviews/components/Review.jsx
@@ -2,12 +2,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { formatDistanceToNow, parseISO } from 'date-fns';
+import Stars from 'App/Stars.jsx'
 
 const StyledReview = styled.div`
   border: 2px solid black;
   margin: 10px;
 `;
-const Stars = styled.p`
+const Star = styled.p`
   padding-top: 10px;
   float: left;
   padding-left: 10px;
@@ -18,7 +19,7 @@ const User = styled.p`
   padding-right: 10px;
 `;
 const Title = styled.h2`
-  padding-top: 10px;
+  padding-top: 15px;
   font-size: 150%;
 `;
 const Buttons = styled.button`
@@ -30,20 +31,22 @@ const Buttons = styled.button`
 function Review({ review }) {
   let date;
   let rec = '';
-  function handleReviewClick(e, type) {
+  const handleReviewClick = (e, type) => {
     e.preventDefault();
     // send request to review.review_id, type (helpful or report)
-  }
-  function parseData() {
+  };
+  const parseData = () => {
     date = formatDistanceToNow(parseISO(review.date));
     if (review.recommend) {
       rec = '✅';
     }
-  }
+  };
   parseData();
   return (
     <StyledReview>
-      <Stars>{review.rating}</Stars>
+      <Star>
+        <Stars rating={review.rating} />
+      </Star>
       {/* need to add fn to display stars */}
       <User>
         {`${rec} ${review.reviewer_name}, ${date} ago`}
