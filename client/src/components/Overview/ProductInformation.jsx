e7568fa07427b4e706aeae50adae5aa1364869c9
Shannon Largman <shannon.largman@gmail.com>
2022-05-24 18:58:05 -0700

    Move StyleSelector and AddToCart to Overview

--- client/src/components/Overview/ProductInformation.jsx
+++ client/src/components/Overview/ProductInformation.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StyleSelector from 'Overview/StyleSelector.jsx';
-import AddToCart from 'Overview/AddToCart.jsx';
 
 /**
  * Shows general product information
  */
-function ProductInformation({
-  product,
-  styles,
-  selectedStyleId,
-  handleStyleSelect,
-}) {
-  // TODO: how to avoid optional chaining?
-  const selectedStyle = styles?.find(
-    (style) => style.style_id === selectedStyleId
-  );
-
+function ProductInformation({ product, selectedStyle}) {
   return (
     <div>
       {/*TODO: add rating stars*/}
@@ -33,21 +21,13 @@ function ProductInformation({
       {/*TODO: add Share buttons*/}
       {selectedStyle?.original_price}
       {selectedStyle?.sale_price !== 0 ? selectedStyle?.sale_price : null}
-      <StyleSelector
-        styles={styles}
-        selectedStyleId={selectedStyleId}
-        handleStyleSelect={handleStyleSelect}
-      />
-      <AddToCart />
     </div>
   );
 }
 
 ProductInformation.propTypes = {
   product: PropTypes.object.isRequired,
-  styles: PropTypes.array.isRequired,
-  selectedStyleId: PropTypes.number.isRequired,
-  handleStyleSelect: PropTypes.func.isRequired,
+  selectedStyle: PropTypes.object.isRequired,
 };
 
 export default ProductInformation;
