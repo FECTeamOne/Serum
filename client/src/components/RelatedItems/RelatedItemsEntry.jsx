03d7d9c98b9513022707d7729f1a0f561316e0be
hangyin2020 <hangyin2010@gmail.com>
2022-05-27 10:08:50 -0700

    added related img from API

--- client/src/components/RelatedItems/RelatedItemsEntry.jsx
+++ client/src/components/RelatedItems/RelatedItemsEntry.jsx
@@ -25,11 +25,13 @@ const Action = styled.button`
   border-radius: 5px;
 `;
 
-function RelatedItemsEntry({ item, onOpen, onCompare }) {
+function RelatedItemsEntry({ img, item, onOpen, onCompare }) {
+  // console.log(img);
+
   return (
     <Container>
       <img
-        src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+        src={img}
         width="200"
         height="300"
         alt={`Thumbnail for related item ${item.name}`}
@@ -52,6 +54,7 @@ RelatedItemsEntry.propTypes = {
   item: PropTypes.object.isRequired,
   onOpen: PropTypes.object.isRequired,
   onCompare: PropTypes.object.isRequired,
+  img: PropTypes.object.isRequired,
   // size: PropTypes.number.isRequired,
 };
 
--- client/src/components/RelatedItems/RelatedItemsList.jsx
+++ client/src/components/RelatedItems/RelatedItemsList.jsx
@@ -1,28 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import styled from 'styled-components';
 import RelatedItemsEntry from 'RelatedItems/RelatedItemsEntry.jsx';
 import Carousel from 'App/Carousel.jsx'
 import { products } from 'tests/testData.js';
 import Modal from './Modal.jsx';
+import axios from 'axios';
 
 // need to pass {currentItemId} into the props
-function RelatedItemsList({ currentItem }) {
+function RelatedItemsList({ currentItemId }) {
   const [showModal, setShowModal] = useState(false);
+  const [arrayRelated, setArrayRelated] = useState([]);
+  const [allRelated, setAllRelated] = useState([]);
   const [currentRelated, setCurrentRelated] = useState(0);
 
-  // get related items from the API; [array of item id]
-  // get related items detail from the API; [{item 1}, {item 2}..]
-  const relatedItems = products.slice();
-  // change the product id for test only;
-  // relatedItems.forEach((item, index) => { item.id = index; });
-  console.log(relatedItems);
-  // detail of Modal
+  const relatedItemsStyles = [];
+
+  const fetchData = async () => {
+    const [relatedItemsArray, allProducts] = await Promise.all([axios.get('/products/40346/related'), axios.get('/products')]);
+    const temps = allProducts.data.filter((item) => (relatedItemsArray.data.indexOf(item.id) >= 0));
+    setAllRelated(temps);
+    setArrayRelated(relatedItemsArray.data);
+    relatedItemsArray.data.forEach((itemId) => {
+      relatedItemsStyles.push(axios.get(`/products/${itemId}/styles`));
+    });
+    const test = await Promise.all(relatedItemsStyles);
+    const allRelatedCopy = temps.slice();
+    test.forEach((item) => {
+      allRelatedCopy.forEach((product) => {
+        const tmpProduct = product;
+        if (tmpProduct.id.toString() === item.data.product_id) {
+          tmpProduct.img = item.data.results[0].photos[0].url;
+          console.log('product in copy: ', tmpProduct);
+        }
+      });
+    });
+    setAllRelated(allRelatedCopy);
+  };
+
+  useEffect(() => { fetchData(); }, []);
+
   const currentItemChars = [{ char1: 'String' }, { char2: 'String' }, { char3: 'String' }];
   const currentRelatedChars = [{ char1: 'String' }, { char2: 'String' }, { char3: 'String' }];
 
-  const relatedItemsEntries = relatedItems.map((item) => (
+  const relatedItemsEntries = allRelated.map((item) => (
     <RelatedItemsEntry
       key={item.id}
+      img={item.img}
       item={item}
       onOpen={() => setShowModal(true)}
       onCompare={() => setCurrentRelated(item.id)}
