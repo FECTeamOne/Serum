e3c96f07b52e3987826ed938cc1a03a2702f04e3
Shannon Largman <shannon.largman@gmail.com>
2022-05-27 13:34:38 -0700

    feat: add global styles and Navbar

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import Navbar from 'App/Navbar.jsx';
 import Reviews from 'Reviews/Reviews.jsx';
 import Overview from 'Overview/Overview.jsx';
-import RelatedItemsList from 'RelatedItems/RelatedItemsList.jsx'
+import RelatedItemsList from 'RelatedItems/RelatedItemsList.jsx';
+import GlobalStyle from '../../globalStyles.js';
 
-const Container = styled.div`
-  text-align: center;
+const Wrapper = styled.div`
+  margin:auto;
+  width: fit-content;
 `;
 
 function App() {
   return (
-    <Container>
-      <Overview productId={11}/>
-      <Reviews />
-      <RelatedItemsList />
-    </Container>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <Navbar />
+        <Overview productId={40344}/>
+        <Reviews
+        <RelatedItemsList
+      </Wrapper>
+    </>
   );
 }
 
