01f699384013e64d6d2b36ad14fb2cbdff17e362
hangyin2020 <hangyin2010@gmail.com>
2022-05-29 10:40:58 -0700

    change the relatedItemlist and outfitItemList to jsx to import in relatedItems

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import RelatedItemsList from './RelatedItemsList';
-import OutfitItemsList from './OutfitItemsList';
+import RelatedItemsList from './RelatedItemsList.jsx';
+import OutfitItemsList from './OutfitItemsList.jsx';
 
 function RelatedItems({ productId }) {
   return (
