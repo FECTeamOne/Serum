8e314b005ade2a087769b24e2e98481238c0041d
Shannon Largman <shannon.largman@gmail.com>
2022-05-29 22:22:13 -0700

    docs(Carousel): add docs for Carousel

--- client/src/components/shared/Carousel.jsx
+++ client/src/components/shared/Carousel.jsx
@@ -4,34 +4,17 @@ import styled from 'styled-components';
 import Button from 'shared/Button.jsx';
 import ArrowIcon from 'assets/ArrowIcon.jsx';
 
-const StyledCarousel = styled.div`
-  display: flex;
-  flex-direction: ${({ direction }) => direction};
-  gap: ${({ gap }) => gap};
-`;
-
-const CarouselItem = styled.div`
-  display: ${({ visible }) => (visible ? '' : 'none')};
-`;
-
-const CarouselButton = styled(Button)`
-  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
-`;
-
-// TODO: update docs
 /**
  * Displays a set of items with buttons to scroll through them
- * @param {Array}  items The items to be displayed
- * @param {Number} size  How many items to display at once
  */
 function Carousel({
   items,
   size,
   scrollIndex,
-  direction = 'row',
+  handleScroll,
+  direction,
   arrowWidth,
   arrowHeight,
-  handleScroll = () => {},
 }) {
   const placeInRange = (index) => {
     const adjustedIndex = Math.min(index, items.length - size + 1);
@@ -48,13 +31,11 @@ function Carousel({
         onClick={() => { handleScroll(placeInRange(adjustedScrollIndex - size), size); }}
         visible={adjustedScrollIndex !== 0}
       >
-
         <ArrowIcon
           iconWidth={arrowWidth}
           iconHeight={arrowHeight}
           rotation={2 + Number(direction === 'column')}
         />
-
       </CarouselButton>
 
       {items.map((item, i) => (
@@ -71,13 +52,11 @@ function Carousel({
         onClick={() => { handleScroll(placeInRange(adjustedScrollIndex + size), size); }}
         visible={adjustedScrollIndex < items.length - size}
       >
-
         <ArrowIcon
           iconWidth={arrowWidth}
           iconHeight={arrowHeight}
           rotation={Number(direction === 'column')}
         />
-
       </CarouselButton>
 
     </StyledCarousel>
@@ -85,9 +64,60 @@ function Carousel({
 }
 
 Carousel.propTypes = {
+  /** The items to be displayed in the carousel */
   items: PropTypes.arrayOf(PropTypes.element).isRequired,
+  /** How many items to display at once */
   size: PropTypes.number.isRequired,
+  /** The index in items of the first item that should appear in the
+   * carousel. This prop should be used to make Carousel behave as a
+   * controlled component.
+   */
+  scrollIndex: PropTypes.number.isRequired,
+  /**
+   * Function to be called when one of the carousel buttons is
+   * clicked and the carousel should scroll. When this occurs
+   * handleScroll is called with two arguments: index and size. Index
+   * is the value that scrollIndex should take to reflect the button
+   * click and size is the value of size, provided for convenience.
+   * Passing handleScroll the following function suffices to provide
+   * the basic carousel behavior: (index) => { setScrollIndex(index); },
+   * where setScrollIndex is the setter for the state variable passed
+   * to scrollIndex.
+   */
+  handleScroll: PropTypes.func.isRequired,
+  /**
+   * Carousel direction. Use 'row' for horizontal and 'column' for
+   * vertical.
+   */
   direction: PropTypes.string,
+  /**
+   * Width to be used for the arrow icon, e.g. "100px" or "var(--size-3)"
+   */
+  arrowWidth: PropTypes.string,
+  /**
+   * Height to be used for the arrow icon, e.g. "100px" or "var(--size-3)"
+   */
+  arrowHeight: PropTypes.string,
 };
+
+Carousel.defaultProps = {
+  direction: 'row',
+  arrowWidth: 'var(--size-2)',
+  arrowHeight: 'var(--size-2)',
+};
+
+const StyledCarousel = styled.div`
+  display: flex;
+  flex-direction: ${({ direction }) => direction};
+  gap: ${({ gap }) => gap};
+`;
+
+const CarouselItem = styled.div`
+  display: ${({ visible }) => (visible ? '' : 'none')};
+`;
+
+const CarouselButton = styled(Button)`
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
+`;
 
 export default Carousel;
