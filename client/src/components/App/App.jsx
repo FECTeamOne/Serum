84c4b8554d5230a84c4e7fc1ab72f6e11d224884
zman811 <sunburnzman@gmail.com>
2022-06-02 16:04:17 -0600

    Added in shared axios reqs and refactored reviews to use them

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Navbar from 'App/Navbar.jsx';
 import Reviews from 'Reviews/Reviews.jsx';
 import Overview from 'Overview/Overview.jsx';
+import axios from 'axios';
 import RelatedItems from 'RelatedItems/RelatedItems.jsx';
 import { StarIconFills } from 'assets/StarIcon.jsx'
 import GlobalStyle from '../../globalStyles.js';
 
 function App() {
+  const productId = 40344; // * temp till we use react router
+  const [reviewsMetadata, setReviewsMetaData] = useState('');
+  const [productData, setProductData] = useState('');
+  const [productStyles, setProductStyles] = useState('');
+  useEffect(() => {
+    axios.get(`/reviews/meta?product_id=${productId}`)
+      .then((res) => { setReviewsMetaData(res.data); })
+      .catch((err) => console.log(err));
+    axios.get(`/products/${productId}`)
+      .then((res) => { setProductData(res.data); })
+      .catch((err) => console.log(err));
+    axios.get(`/products/${productId}/styles`)
+      .then((res) => { setProductStyles(res.data); })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <StarIconFills />
@@ -16,7 +32,7 @@ function App() {
       <Wrapper>
         <Navbar />
         <Overview productId={40344} />
-        <Reviews productId={40344} />
+        <Reviews reviewsMetadata={reviewsMetadata} productData={productData} />
         <RelatedItems productId={40344} />
       </Wrapper>
     </>
