ff8582ef16881dfe0f172229dd0b2e83f85712a4
Shannon Largman <shannon.largman@gmail.com>
2022-06-03 16:11:25 -0700

    fix: fix bg height

--- client/src/components/App/Home.jsx
+++ client/src/components/App/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
 
 const StyledHome = styled.div`
   width: 100vw;
-  height: 100vw;
+  height: calc(100vh);
   background-image: url('/assets/home-bg.jpg');
   background-position: center;
   background-size: cover;
