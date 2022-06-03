efadc0d9e0165c6e3754fc21a450e1cb1ad17591
zman811 <sunburnzman@gmail.com>
2022-06-03 11:15:33 -0600

    Added a more button to see more products

--- client/src/components/App/Home.jsx
+++ client/src/components/App/Home.jsx
@@ -3,53 +3,52 @@ import styled from 'styled-components';
 import axios from 'axios';
 import ProductCard from 'shared/ProductCard.jsx';
 import Button from 'shared/Button.jsx';
-import { Link, useNavigate } from 'react-router-dom';
-import { StarIconFills } from 'assets/StarIcon.jsx'
+import { useNavigate } from 'react-router-dom';
+import { StarIconFills } from 'assets/StarIcon.jsx';
 import GlobalStyle from '../../globalStyles.js';
 
 function Home() {
   const [products, setProducts] = useState([]);
   const [productData, setProductData] = useState({});
+  const [page, setPage] = useState(1);
   const nagivate = useNavigate();
   useEffect(() => {
-    axios.get('/products')
-      .then((data) => setProducts(data.data))
+    axios.get(`/products?page=${page}`)
+      .then((data) => setProducts([...products, ...data.data]))
       .catch((err) => console.log(err));
-  }, []);
+  }, [page]);
   useEffect(() => {
-    console.log(products)
     if (!products[0] || !Array.isArray(products)) {
       return;
     }
     const promises = [];
     const data = {};
-    products.forEach((product, i) => {
-      promises.push(axios.get(`/products/${product.id}/styles`)
+    for (let i = (page - 1) * 5; i < products.length; i++) {
+      promises.push(axios.get(`/products/${products[i].id}/styles`)
         .then((res) => {
           if (!data[i]) {
             data[i] = {};
           }
           data[i].photo = res.data.results[0].photos[0].url;
-          data[i].product = product;
         })
         .catch((err) => console.log(err)));
-      promises.push(axios.get(`/reviews/meta?product_id=${product.id}`)
+      promises.push(axios.get(`/reviews/meta?product_id=${products[i].id}`)
         .then((results) => {
           if (!data[i]) {
             data[i] = {};
           }
+          data[i].product = products[i];
           data[i].meta = (results.data);
         })
         .catch((err) => console.log(err)));
-    });
+    }
     Promise.all(promises)
-      .then(() => setProductData(data))
+      .then(() => setProductData({ ...data, ...productData }))
       .catch(() => {});
   }, [products]);
   if (!productData['0']) {
-    return <div>laoding...</div>;
+    return <div>loading...</div>;
   }
-  console.log(Object.values(productData));
   return (
     <div>
       <GlobalStyle />
@@ -65,6 +64,7 @@ function Home() {
             />
           </Button>
         ))}
+        <Button onClick={() => setPage(page + 1)}>More</Button>
       </div>
     </div>
   );
